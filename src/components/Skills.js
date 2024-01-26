232a4945c4f68837552fc8732d4160c03a5150ef
Gajraj Singh Bhati <gsbgajraj@gmail.com>
2024-01-26 17:07:30 +0530

    language modified

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,7 +35,7 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>I am a web developer and Graphic designer.<br></br> My skills incude programming languages C++ and Javascript <br /> Tech stack includes HTML,CSS,Javascript, ReactJS, NodeJs.</p>
+                        <p>I am a web developer and Graphic designer.<br></br> My skills incude programming languages C++,Python Javascript <br /> Tech stack includes HTML,CSS,Javascript, ReactJS, NodeJs.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
